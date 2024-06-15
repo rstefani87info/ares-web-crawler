d92405f66da1691906a55a303956c74cf9e39447
RS_ict <r.stefani87.informatica@gmail.com>
2024-06-16 01:58:09 +0200

    fix

--- crawler.js
+++ crawler.js
@@ -1,73 +1,58 @@
 import request from "request";
 import path from "path";
-import { Builder } from "selenium-webdriver";
-import { parseUrl } from "@ares/web-ui/ml.js";
+import axios from "axios";
+import { Builder, By, Key, until } from "selenium-webdriver";
+import { parseUrl, parseCode } from "@ares/web-ui/ml.js";
+import {findPropValueByAlias} from '@ares/core/objects.js'
 // import {cssSelect, cssFilter} from 'css-select';
 
 
 class Crawler {
-
-
   constructor(urlFilters = {}) {
     this.urlFilters = urlFilters;
     this.urlMap = {};
   }
 
-  /**
-   *
-   * @param {*} browserName - desired browser (chrome, firefox, edge)
-   */
-  async initBrowser(browserName) {
-    const browser = await new Builder().forBrowser(browserName).build();
-    return browser;
+  async getUrlTypeFromResponse( 
+    url, 
+    method = "GET",
+    headers = {},
+    payload = {} ){
+
+      method=method.toLowerCase();
+      if(method==="get"){
+        url = url + (url.includes("?") ? "&" : "?") + new URLSearchParams(payload).toString();
+      }
+      const params =[url,{ headers: headers }]
+      if(method!=="get") params[1].data = payload;
+      let response =  await axios[method](...params); 
+      let extension = path.extname(url.split("?", 1)[0]).replace(".", "");
+      const contentType = response
+        ? response.headers["content-type"] ?? "text/html"
+        : "text/html";
+      extension = contentType.split("/")[1];
+      extension = extension.toUpperCase();
+      return extension;
   }
 
-  async crawlUrl(url, browser) {
-    if (!this.urlMap[url]) {
-      let done = false;
-      let response = null;
-      request(url, async (er, res, body) => {
-        response = res;
-        response = await parseUrl(url);
+  async crawlUrl(url) {
+    let urlFilter = findPropValueByAlias(this.urlFilters, url);
+    if (!this.urlMap[url] && urlFilter) {
+      
+      const method = (urlFilter?.method) ?? "GET";
+      const headers= (urlFilter?.headers) ?? {};  
+      const payload= (urlFilter?.payload) ?? {};
+      const onload= (urlFilter?.onload) ?? null; 
+      const options= (urlFilter?.options) ?? {};
+       
+      const extension = await this.getUrlTypeFromResponse(url,method,headers,payload);
 
-        if (browser) {
-          if (typeof browser === "string") {
-            browser = await initBrowser(browser);
-          }
-          if (browser instanceof Object) {
-            await browser.get(url);
-            response = {
-              headers: response.headers,
-              body: await driver.getPageSource(),
-            };
-          }
-        }
-
-        for (const key in this.urlFilters) {
-          if (key && this.urlFilters[key]?.analyzeUrl) {
-            done =
-              (await this.urlFilters[key].analyzeUrl(url)) &&
-              this.urlFilters[key]?.final;
-            if (done) break;
-          }
-        }
-        if (!done) {
-          let extension = path.extname(url.split("?", 1)[0]).replace(".", "");
-          const contentType = response
-            ? response.headers["content-type"] ?? "text/html"
-            : "text/html";
-          extension = contentType.split("/")[1];
-          extension = extension.toUpperCase();
-          if (response && response.body) {
-            if (this["crawl" + extension]) {
-              this["crawl" + extension](url, response);
-            } else {
-              console.log(`No crawler for extension ${extension}`);
-            }
-          }
-        }
-        this.urlMap[url] = true;
-      });
+      if (this["crawl" + extension]) {
+        this["crawl" + extension](url, method, headers, payload, onload, options, urlFilter);
+      } else {
+        console.log(`No crawler for extension ${extension}`);
+      }
+      this.urlMap[url] = true;
     }
   }
 
@@ -76,21 +61,38 @@ class Crawler {
   /**
    *
    * @param {*} url - the url of the current page
-   * @param {*} response - the response of the current request
    */
-  async crawlHTML(url, response) {
-    const { $ } = parseCode(response.body, response.header);
-    const root = $("*:nth-child(1)")[0];
-    this.analyzeMLElement(url, $, root);
-    const srcElement = $("[src]");
-    this.analyzeLinkerTags(url, srcElement, $, "src");
-    const hrefElement = $("[href]");
-    this.analyzeLinkerTags(url, hrefElement, $, "href");
-
-    const sitemapLink = $('link[rel="sitemap"]').attr("href");
-    if (sitemapLink) {
-      const sitemapUrl = new URL(sitemapLink, url).toString();
-      await this.crawlUrl(sitemapUrl);
+  async crawlHTML(
+    url, 
+    method = "GET",
+    headers = {},
+    payload = {},
+    onload = null,
+    options = {userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", runScript: "dangerously"},
+    urlFilter = null
+  ) {
+     const jdom = await parseUrl(
+        url,
+        method ,
+        headers,
+        payload,
+        onload,
+        options
+     );
+     const $ = jdom.window.aReS.$;
+    const root = jdom.window.aReS.$("*:nth-child(1)")[0];
+    if(urlFilter?.analyzeMLElement)urlFilter.analyzeMLElement(url, $, root);
+    else this.analyzeMLElement(url, $, root, urlFilter);
+    if(!urlFilter?.excludeCalledResources){
+      const srcElement = $("[src]");
+      this.analyzeLinkerTags(url, srcElement, $, "src");
+      const hrefElement = $("[href]");
+      this.analyzeLinkerTags(url, hrefElement, $, "href");
+      const sitemapLink = $('link[rel="sitemap"]').attr("href");
+      if (sitemapLink) {
+        const sitemapUrl = new URL(sitemapLink, url).toString();
+        await this.crawlUrl(sitemapUrl);
+      }
     }
   }
 
@@ -113,41 +115,51 @@ class Crawler {
     }
   }
 
-  async analyzeMLElement(url, $, element) {
+  async analyzeMLElement(url, $, element, urlFilter = null) {
     let done = false;
-    const elementHash = cheerio.html(element);
-    for (const key in this.urlFilters[url]?.selectors ?? []) {
+    const elementHash = element.outerHTML;
+    for (const key in ((urlFilter?.selectors) ?? [])) {
       const selectedList = $(key);
       const selected = selectedList.filter(
-        (x) => cheerio.html(selectedList[x]) === elementHash
+        (x) => selectedList[x].outerHTML === elementHash
       );
       if (
         selected.length &&
-        this.urlFilters[url].selectors[key]?.analyzeMLElement
+        await urlFilter.selectors[key]?.analyzeMLElement
       ) {
-        done =
-          (await this.urlFilters[url].selectors[key].analyzeMLElement(
+        // done =
+          (await urlFilter.selectors[key].analyzeMLElement(
             url,
             $,
             element
-          )) && this.urlFilters[url].selectors[key]?.final;
+          )) && !urlFilter.selectors[key]?.skipChildren;
       }
-      let analyzeMLElementChild = this.analyzeMLElementChild;
-      if (
-        typeof this.urlFilters[url].selectors[key]?.analyzeMLElementChildren ===
-        "function"
-      )
-        analyzeMLElementChild =
-          this.urlFilters[url].selectors[key].analyzeMLElementChild;
-      if (this.urlFilters[url].selectors[key]?.analyzeMLElementChildren) {
-        await analyzeMLElementChild(url, $, element);
+      if ( urlFilter.selectors[key]?.analyzeMLElementChildren !== false ) {
+        await this.analyzeMLElementChild(url, $, element);
       }
-      if (done) break;
+      // if (done) break;
     }
-    if (!done) {
-      const tagName = element.name.toUpperCase();
-      if (this["analyze" + tagName]) {
-        this["analyze" + tagName](element);
+    // if (!done) {
+    //   const tagName = element.tagName.toUpperCase();
+    //   if (this["analyze" + tagName]) {
+    //     this["analyze" + tagName](element);
+    //   }
+    // }
+  }
+
+  async analyzeMLElementChild(url, $, element) {
+    let textValue = [];
+    for (const child of element.childNodes){
+      
+      if(child.nodeType === 3){
+        textValue.push(child.data);
+      }else if(child.nodeType === 8) {
+        // TODO: analyze comments
+      }else if (child.nodeType === 7) {
+        // TODO: analyze processing instructions
+      }
+      else if(child.tagName){
+         this.analyzeMLElement(url, $, child);
       }
     }
   }
